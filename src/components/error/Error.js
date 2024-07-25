c0c36417ed73c01aa5a6392ba0c8de790ad7c87c
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-25 19:11:56 +0200

    Add Error messages

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom';
 
 const Error = () => {
 
+    const errorTitle = "System Error";
+    const errorText = "It seems that there is some problem with system. We will try to resolve it as soon as possible. Sorry for the inconvenience.";
+
     return(
         <div className="row h-100 justify-content-center align-items-center">
             <div>	
@@ -9,10 +12,10 @@ const Error = () => {
                     <h1><span className="error-sign">Ooops!</span></h1>
                 </div>
                 <div className="row justify-content-center">
-                    <h2 className="error-title" > {{errorTitle}} </h2>
+                    <h2 className="error-title" > {errorTitle} </h2>
                 </div>
                 <div className="row justify-content-center">
-                    <p> {{errorText}} </p>
+                    <p> {errorText} </p>
                 </div>
                 <div className="row justify-content-center">
                     <Link to="/" className="btn py-3 px-4 btn-primary">Go To List</Link>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import List from './components/list/List';
 import About from './components/about/About';
+import Error from './components/error/Error';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,6 +11,7 @@ root.render(
     <Routes>
       <Route path="/" element={<List/>} />
       <Route path="/about" element={<About/>} />
+      <Route path="*" element={<Error/>} />
     </Routes>
   </BrowserRouter>
 );
