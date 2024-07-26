53845a8cd9d8edf2fd535b60cb078d0668b225ec
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-26 17:46:14 +0200

    Improvement of Article component

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -17,6 +17,14 @@ const Article = () => {
         };
       }, []);
 
+    const title = "Title";
+    const date = "Date";
+    const author = "Author";
+    const description = "Description (Inner HTML)";
+    const template = "CONTENT";
+    const content = "Content (Inner HTML)";
+    const url = "https://www.google.com";
+
     return(
         <div id="colorlib-page">
             <form>
@@ -27,21 +35,19 @@ const Article = () => {
                             
                                 <span class=" d-none d-xl-block">
                                     <p class="blog-img">
-                                        <a routerLink="">
-                                        <img src="https://chrisblog.s3-eu-west-1.amazonaws.com/common/chris_blog.png" alt="" class="img-fluid"/>
-                                        </a>
+                                        <Link to="/">
+                                            <img src="https://chrisblog.s3-eu-west-1.amazonaws.com/common/chris_blog.png" alt="" class="img-fluid"/>
+                                        </Link>
                                     </p>
                                 </span>	
 
                                 <div class="nav d-flex align-items-center full-height">
-                                    {/* TODO */}
-                                    <a routerLink="">
+                                    <Link to="/">
                                         <span class="nav-item">Home</span>
-                                    </a>
-                                    {/* TODO */}
-                                    <a routerLink="/about">
+                                    </Link>
+                                    <Link to="/about">
                                         <span class="nav-item">About Me</span>
-                                    </a>
+                                    </Link>
                                 </div>
 
                                 <div class="row articleSection pt-md-4 articleBanner d-flex align-items-center justify-content-center full-height"	    					
@@ -49,15 +55,12 @@ const Article = () => {
                                     >
                                     <div class="col-lg-12 px-md-7 py-7" >
                                         <div class="row articleSection d-flex align-items-center justify-content-center full-height ">
-                                            {/* TODO */}
-                                            <div class="mb-3 articleBannerText" > {{article?.title}} </div>
+                                            <div class="mb-3 articleBannerText" > {title} </div>
                                         </div>
                                         <div class="row articleSection meta-wrap">
                                             <p>
-                                                {/* TODO */}
-                                                <span class="icon-wrap"><i class="icon-calendar mr-2"></i> {{article?.date}} </span>
-                                                {/* TODO */}
-                                                <span class="icon-wrap"><i class="icon-person mr-2"></i> {{article?.title}} </span>                                                
+                                                <span class="icon-wrap"><i class="icon-calendar mr-2"></i> {date} </span>
+                                                <span class="icon-wrap"><i class="icon-person mr-2"></i> {title} </span>                                                
                                             </p>
                                         </div>
                                     </div>
@@ -66,72 +69,71 @@ const Article = () => {
                                 <span class="d-none d-xl-block">
                                     <div class="avatarIcon">
                                         <div class="avatar">
-                                            <img th:src="@{https://chrisblog.s3-eu-west-1.amazonaws.com/common/none.png}" alt=""/>
+                                            <img src="https://chrisblog.s3-eu-west-1.amazonaws.com/common/none.png" alt=""/>
                                         </div>
-                                        {/* TODO */}
-                                        <span class="author_name">By: <b> {{article?.author}} </b> </span>
+                                        <span class="author_name">By: <b> {author} </b> </span>
                                     </div>
                                 </span>	
 
                                 <hr class="line d-none d-xl-block"/>						
                                     
                                 <h2 class="mb-3 mt-5 articleSectionTitle">Introduction</h2>
-                                <div class="row articleSection pt-md-4" [innerHtml]="article?.description"></div>
+                                <div class="row articleSection pt-md-4" > {description} </div>
                                 
                                 <p class="article-img">
                                     <img src="https://chrisblog.s3-eu-west-1.amazonaws.com/articles/article-{{article?.id}}/main.jpg" alt="" class="img-fluid"/>
                                 </p>
 
-                                {/* TODO */}
-                                <span if="article?.template == 'CONTENT'">
-                                    <h2 class="mb-3 mt-5 articleSectionTitle">Description</h2>	
-                                    {/* TODO */}
-                                    <div class="row articleSection pt-md-4" innerHtml="article?.content"></div>	
-                                </span>
+                                if ({template} == 'CONTENT') {
+                                    <span>
+                                        <h2 class="mb-3 mt-5 articleSectionTitle">Description</h2>	
+                                        <div class="row articleSection pt-md-4" > {content} </div>	
+                                    </span>
+                                }
 
-                                {/* TODO */}
-                                <span if="article?.template == 'LINK_ALBUM'">
-                                    <h2 class="mb-3 mt-5 articleSectionTitle">Google Album</h2>				            
-                                    <div class="row articleSection pt-md-4">
-                                        Pictures from this area you can find in my
-                                        {/* TODO  */}
-                                        <a href="{{article?.url}}" target="blank">Google Album</a>.
-                                    </div>
-                                </span>	
+                                if ({template} == 'LINK_ALBUM') {
+                                    <span>
+                                        <h2 class="mb-3 mt-5 articleSectionTitle">Google Album</h2>				            
+                                        <div class="row articleSection pt-md-4">
+                                            Pictures from this area you can find in my
+                                            <a href="{url}" target="blank">Google Album</a>.
+                                        </div>
+                                    </span>	
+                                }
 
-                                {/* TODO */}
-                                <span if="article?.template == 'LINK_DOCUMENT'">
-                                    <h2 class="mb-3 mt-5 articleSectionTitle">Google Document</h2>				            
-                                    <div class="row articleSection pt-md-4">
-                                        More details about this topic you can find in my 
-                                        {/* TODO */}
-                                        <a href="{{article?.url}}" target="blank">Google Document</a>.
-                                    </div> 
-                                </span> 
+                                if ({template} == 'LINK_DOCUMENT') {
+                                    <span>
+                                        <h2 class="mb-3 mt-5 articleSectionTitle">Google Document</h2>				            
+                                        <div class="row articleSection pt-md-4">
+                                            More details about this topic you can find in my 
+                                            <a href="{url}" target="blank">Google Document</a>.
+                                        </div> 
+                                    </span> 
+                                }
 
-                                {/* TODO */}
-                                <span if="article?.template == 'LINK_GITHUB'">
-                                    <h2 class="mb-3 mt-5 articleSectionTitle">Github Repository</h2>				            
-                                    <div class="row articleSection pt-md-4">
-                                        Source Code with explanation you can find in my 
-                                        {/* TODO */}
-                                        <a href="{{article?.url}}" target="blank">Github Repository</a>.
-                                    </div>
-                                </span>
+                                if ({template} == 'LINK_GITHUB') {
+                                    <span>
+                                        <h2 class="mb-3 mt-5 articleSectionTitle">Github Repository</h2>				            
+                                        <div class="row articleSection pt-md-4">
+                                            Source Code with explanation you can find in my 
+                                            {/* TODO */}
+                                            <a href="{{article?.url}}" target="blank">Github Repository</a>.
+                                        </div>
+                                    </span>
+                                }
 
-                                {/* TODO */}
-                                <span if="article?.template == 'LINK_YOUTUBE'">
-                                    <h2 class="mb-3 mt-5 articleSectionTitle">YouTube Channel</h2>				            
-                                    <div class="row articleSection pt-md-4">
-                                        More details about this topic you can find on my
-                                        {/* TODO */}
-                                        <a href="{{article?.url}}" target="blank">YouTube Channel</a>.
-                                    </div>
-                                </span> 
+                                if ({template} == 'LINK_YOUTUBE') {
+                                    <span>
+                                        <h2 class="mb-3 mt-5 articleSectionTitle">YouTube Channel</h2>				            
+                                        <div class="row articleSection pt-md-4">
+                                            More details about this topic you can find on my
+                                            <a href="{url}" target="blank">YouTube Channel</a>.
+                                        </div>
+                                    </span>
+                                } 
 
                                 <div class="form-group article-button">
-                                    {/* TODO */}
-                                    <a routerLink="" click="onClick()" class="btn py-3 px-4 btn-primary">Go Back</a>
+                                    <Link to="/" class="btn py-3 px-4 btn-primary">Go Back</Link>
                                 </div>       
                             
                             </div>
