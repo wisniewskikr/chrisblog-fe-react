05fac32897355482b116acb437d20f65da8e418a
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-29 19:31:57 +0200

    Implement route redirect

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import List from './components/list/List';
 import About from './components/about/About';
 import Error from './components/error/Error';
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<List/>} />
+      <Route path="/" element={<Navigate to="/category/0/sorting/date_decreasing/page/1" replace />} />
       <Route path="/category/:categoryId/sorting/:sorting/page/:page" element={<List/>} />
       <Route path="/about" element={<About/>} />
       <Route path="/article/:id" element={<Article/>} />
