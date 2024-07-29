8ede2aacb474087b80b201bb467d741ec8759e8b
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-29 17:12:23 +0200

    Update ListMain

--- src/components/list/list-main/ListMain.js
+++ src/components/list/list-main/ListMain.js
@@ -2,12 +2,26 @@ import ListMainArticles from "./list-main-articles/ListMainArticles";
 import ListMainPagination from "./list-main-pagination/ListMainPagination";
 import ListMainSorting from "./list-main-sorting/ListMainSorting";
 
-const ListMain = () => (
-    <>
-        <ListMainSorting/>
-        <ListMainArticles/>
-        <ListMainPagination/>
-    </>
-);
+const ListMain = () => { 
+
+    let articles = [];
+    let categoryId = null;
+    let tagId  = null;
+    let page = null;
+    let sorting = null;
+    let searchText = null;
+    let pages = [];
+    let disablePrevious = false; 
+    let disableNext = false;
+    
+    return (
+        <>
+            <ListMainSorting/>
+            <ListMainArticles/>
+            <ListMainPagination/>
+        </>
+    );
+
+}
 
 export default ListMain;
