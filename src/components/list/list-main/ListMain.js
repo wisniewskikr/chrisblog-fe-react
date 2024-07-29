0399b92a88c0ad1f2170f643307c44c88710e8d1
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-29 08:55:12 +0200

    Init ListMainSorting

--- src/components/list/list-main/ListMain.js
+++ src/components/list/list-main/ListMain.js
@@ -1,8 +1,10 @@
 import ListMainArticles from "./list-main-articles/ListMainArticles";
 import ListMainPagination from "./list-main-pagination/ListMainPagination";
+import ListMainSorting from "./list-main-sorting/ListMainSorting";
 
 const ListMain = () => (
     <>
+        <ListMainSorting/>
         <ListMainArticles/>
         <ListMainPagination/>
     </>
---
+++ src/components/list/list-main/list-main-sorting/ListMainSorting.js
@@ -0,0 +1,9 @@
+const ListMainSorting = () => {
+
+    return (
+        <h2>List Main Sorting</h2>
+    );
+
+}
+
+export default ListMainSorting;
