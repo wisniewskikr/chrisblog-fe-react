8f11eedbbda22137b5a11ebaf032178ba6eed5a5
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-30 08:58:28 +0200

    Init Article return

--- src/components/list/list-main/list-main-articles article/ListMainArticlesArticle.js
+++ src/components/list/list-main/list-main-articles article/ListMainArticlesArticle.js
@@ -15,7 +15,27 @@ const ListMainArticlesArticle = (props) => {
     }, []);
 
     return (
-        <h2>List Main Articles Article with id: {props.article.id}</h2>
+        <div className="col-md-12">
+            <div className="blog-entry ftco-animate d-md-flex">
+                {/* TODO */}
+                <a href="#" className="img img-2" style="background-image: url(https://chrisblog.s3-eu-west-1.amazonaws.com/articles/article-{{article?.id}}/main.jpg)"></a>
+                <div className="text text-2 pl-md-4">
+                    <h3 className="mb-2"><a href="#"><span> {props.article.title} </span></a></h3>
+                    <div className="meta-wrap">
+                        <p className="meta">
+                            <span><i className="icon-calendar mr-2"></i><span> {props.article.date} </span></span>
+                            <span><i className="icon-person mr-2"></i><span> {props.article.author} </span></span>
+                            {/* TODO*/}
+                            {/* <span><i className="icon-folder mr-2"></i><span> {{article.category.name}} </span></span> */}
+                        </p>
+                    </div>
+                    {/* TODO */}
+                    <p className="mb-4" [innerHtml]="article?.description" ></p>
+                    {/* TODO */}
+                    <p><a routerLink="/article/{{article?.id}}" className="btn-custom">Read More <span className="ion-ios-arrow-forward"></span></a></p>
+                </div>
+            </div>
+        </div>
     );
 
 }
