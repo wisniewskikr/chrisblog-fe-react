1c765c2ff68d5a69976bebf2f66212a05fce7156
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-30 08:54:22 +0200

    Add useEffect to Article

--- src/components/list/list-main/list-main-articles article/ListMainArticlesArticle.js
+++ src/components/list/list-main/list-main-articles article/ListMainArticlesArticle.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 const ListMainArticlesArticle = (props) => {
+
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = './../../../../../assets/js/main.js';
+        script.async = true;
+    
+        document.body.appendChild(script);
+    
+        return () => {
+          document.body.removeChild(script);
+        };
+    }, []);
 
     return (
         <h2>List Main Articles Article with id: {props.article.id}</h2>
