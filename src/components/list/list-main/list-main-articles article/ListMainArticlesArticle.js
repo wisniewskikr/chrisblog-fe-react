7ff16e6e5fec283f5ea8852b269d9e562331076e
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-07-30 12:12:33 +0200

    Update Article

--- src/components/list/list-main/list-main-articles article/ListMainArticlesArticle.js
+++ src/components/list/list-main/list-main-articles article/ListMainArticlesArticle.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const ListMainArticlesArticle = (props) => {
 
@@ -14,11 +15,17 @@ const ListMainArticlesArticle = (props) => {
         };
     }, []);
 
+    const inlineStyles = {
+        backgroundImage: {
+            backgroundImage: `url(https://chrisblog.s3-eu-west-1.amazonaws.com/articles/article-${props.article.id}/main.jpg)`,
+        },
+    };
+
     return (
         <div className="col-md-12">
             <div className="blog-entry ftco-animate d-md-flex">
-                {/* TODO */}
-                <a href="#" className="img img-2" style="background-image: url(https://chrisblog.s3-eu-west-1.amazonaws.com/articles/article-{{article?.id}}/main.jpg)"></a>
+                <span className="img img-2" style={inlineStyles.backgroundImage}/>
+                {/* <a href="#" className="img img-2" style={inlineStyles.backgroundImage}></a> */}
                 <div className="text text-2 pl-md-4">
                     <h3 className="mb-2"><a href="#"><span> {props.article.title} </span></a></h3>
                     <div className="meta-wrap">
@@ -29,10 +36,8 @@ const ListMainArticlesArticle = (props) => {
                             {/* <span><i className="icon-folder mr-2"></i><span> {{article.category.name}} </span></span> */}
                         </p>
                     </div>
-                    {/* TODO */}
-                    <p className="mb-4" [innerHtml]="article?.description" ></p>
-                    {/* TODO */}
-                    <p><a routerLink="/article/{{article?.id}}" className="btn-custom">Read More <span className="ion-ios-arrow-forward"></span></a></p>
+                    <p className="mb-4" dangerouslySetInnerHTML={{ __html: props.article.description }} ></p>
+                    <p><Link to="/article/{props.article.id}" className="btn-custom">Read More <span className="ion-ios-arrow-forward"></span></Link></p>
                 </div>
             </div>
         </div>
