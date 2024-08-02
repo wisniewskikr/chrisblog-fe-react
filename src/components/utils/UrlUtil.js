49909cac47ac5f18f12f89bca0a248ce86c95bcd
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-02 12:37:42 +0200

    Implement UrlUtil

---
+++ src/components/utils/UrlUtil.js
@@ -0,0 +1,25 @@
+const UrlUtil = () => {
+
+    function createUrl(pathParams = {}, queryParams = {}) {
+
+        let path = `/category/:categoryId/sorting/:sorting/page/:page`;
+        for (const [key, value] of Object.entries(pathParams)) {
+            path = path.replace(`:${key}`, encodeURIComponent(value));
+        }
+    
+        const searchParams = new URLSearchParams();
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (value !== null && value !== undefined) {
+                searchParams.append(key, value);
+            }
+        }
+    
+        const fullUrl = (![...searchParams.keys()].length) ? `${path}` : `${path}?${searchParams.toString()}`;
+
+        return fullUrl;
+        
+    }
+
+}
+
+export default UrlUtil;
