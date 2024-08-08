71b4fa73772f5fb00c75b2703956922a7df6b479
Krzysztof Wisniewski <krzysztof.wisniewski@contractors.roche.com>
2024-08-08 12:49:19 +0200

    Implement footer

--- src/components/list/list-aside/list-aside-footer/ListAsideFooter.js
+++ src/components/list/list-aside/list-aside-footer/ListAsideFooter.js
@@ -1,7 +1,14 @@
 const ListAsideFooter = () => { 
+
+    const applicationVersion = '1.0.0';
     
     return (
-        <h2>List Aside Footer</h2>
+        <div className="colorlib-footer">				
+	        <p className="pfooter">
+			    Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i className="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>.
+			    Application Version: <span> {applicationVersion} </span>
+			</p>
+        </div>
     );
 
 }
